0960cf6a7015c5e7607775969dfcb806bc1a8b2f
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-03-03 17:46:22 +0530

    Updated number of data to get from API

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 async function fetchAPI() {
   const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
   let retries = 0;
   while (retries < 5) {
